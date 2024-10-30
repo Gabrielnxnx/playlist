cbf5c695454fb376566e917d1e2c978b340107cf
Gabrielnxnx <159441009+Gabrielnxnx@users.noreply.github.com>
2024-10-29 22:53:36 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,3 +9,32 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
+const CACHE_NAME = "my-music-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./images/icon-192x192.png",
+  "./images/icon-512x512.png",
+  "./music/song1.mp3", // Exemplo: adicione outras músicas necessárias
+];
+
+// Instala o Service Worker e armazena os arquivos no cache
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Intercepta as requisições e responde com o cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
